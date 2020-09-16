394386729748208be070c2656da8b1a8ccd46882
Mateusz Karcz <mateusz.karcz@interia.eu>
2020-09-16 23:18:22 +0200

    Lazy loading

--- dotnet/CpiFont/CodePage.cs
+++ dotnet/CpiFont/CodePage.cs
@@ -6,20 +6,28 @@ namespace CpiFont
     {
         Interop.EntryInfo _entry;
         Interop.Stream _stream;
+        List<Font> _fonts;
 
         public CodePage(Interop.Stream stream, Interop.EntryInfo entry)
         {
             _stream = stream;
             _entry = entry;
-            Fonts = new List<Font>();
-            for (int e = 0; e < entry.Fonts; e++) {
-                var font = new Interop.FontInfo{};
-                Interop.cpifont_get_next_font(stream, entry, font);
-                Fonts.Add(new Font(stream, font));
-            }
+            _fonts = new List<Font>();
         }
         
-        public IList<Font> Fonts { get; private set; }
+        public IList<Font> Fonts {
+            get {
+                if (_fonts.Count == 0)
+                {
+                    for (int e = 0; e < _entry.Fonts; e++) {
+                        var font = new Interop.FontInfo{};
+                        Interop.cpifont_get_next_font(_stream, _entry, font);
+                        _fonts.Add(new Font(_stream, font));
+                    }
+                }
+                return _fonts;
+            }
+        }
         public Interop.EntryInfo NativeEntry { get => _entry; }
     }
 }
--- dotnet/CpiFont/CpiFile.cs
+++ dotnet/CpiFont/CpiFile.cs
@@ -7,6 +7,7 @@ namespace CpiFont
     {
         Interop.Stream _stream;
         GCHandle _ctx;
+        List<CodePage> _entries;
 
         public CpiFile(System.IO.Stream stream)
         {
@@ -18,21 +19,7 @@ namespace CpiFont
             _stream.Context = GCHandle.ToIntPtr(_ctx);
 
             Type = Interop.cpifont_get_type(_stream);
-            Entries = new List<CodePage>();
-
-            if (Type != FileType.Unknown)
-            {
-                var entryCount = Interop.cpifont_get_entry_count(_stream);
-                var entry = new Interop.EntryInfo{};
-                for (int e = 0; e < entryCount; e++) {
-                    Interop.cpifont_get_next_entry(_stream, entry);
-                    Entries.Add(new CodePage(_stream, entry));
-                    var next = new Interop.EntryInfo{};
-                    next.NextOffset = entry.NextOffset;
-                    next.FileType = entry.FileType;
-                    entry = next;
-                }
-            }
+            _entries = new List<CodePage>();
         }
 
         ~CpiFile()
@@ -42,7 +29,24 @@ namespace CpiFont
 
         public FileType Type { get; private set; }
 
-        public IList<CodePage> Entries { get; private set; }
+        public IList<CodePage> Entries {
+            get {
+                if (_entries.Count == 0 && Type != FileType.Unknown)
+                {
+                    var entryCount = Interop.cpifont_get_entry_count(_stream);
+                    var entry = new Interop.EntryInfo{};
+                    for (int e = 0; e < entryCount; e++) {
+                        Interop.cpifont_get_next_entry(_stream, entry);
+                        _entries.Add(new CodePage(_stream, entry));
+                        var next = new Interop.EntryInfo{};
+                        next.NextOffset = entry.NextOffset;
+                        next.FileType = entry.FileType;
+                        entry = next;
+                    }
+                }
+                return _entries;
+            }
+        }
     
         public Interop.Stream NativeStream { get => _stream; }
     }
